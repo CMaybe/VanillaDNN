c828b53458aa42e8b4667a8b0ece4666bb84d439
김재겸 <asbsds123@gmail.com>
2021-05-05 05:10:46 +0000

    Add cmake & TC

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include "Math/Matrix/Matrix.hpp"
+#include "Math/Vector/Vector.hpp"
 
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main()
-{
-	cout<<"Test!"<<endl;
+int main(int argc, char **argv) {
+	Matrix<int> m1(3, 3, 0);
+	Matrix<int> m2(3, 3, 0);
+	Vector<int> v(3,2);
+	
+	for(int i = 0;i<3;i++){
+		v(i) = rand()%10;
+		for(int j=0;j<3;j++){
+			m1(i,j) = rand() % 10;
+			m2(i,j) = rand() % 10;
+		}
+	}
+	cout<<v;
+	cout<<m1 + m2;
+	cout<<m1-m2<<endl;
+	cout<<m1*m2<<endl;
+	cout<<m1.transpose()<<endl;
+	
+	
+	cout<< m1 * v;
+	
+	return 0;
 }
