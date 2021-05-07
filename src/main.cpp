195f70249f106b3686618d684c11f7d97482dd47
김재겸 <asbsds123@gmail.com>
2021-05-07 10:06:09 +0000

    test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "Math/Matrix/Matrix.hpp"
 #include "Math/Vector/Vector.hpp"
+#include "DNN/Layer/Layer.hpp"
+#include "DNN/Functions/DNNFunction.hpp"
 
 #include <iostream>
 #include <cstdlib>
@@ -10,6 +12,9 @@ int main(int argc, char **argv) {
 	Matrix<int> m2(3, 3, 0);
 	Vector<int> v(3,2);
 	
+	Layer layer;
+	layer.setActivation(&ACTIVATION_FUNCTION::sigmoid);
+	
 	for(int i = 0;i<3;i++){
 		v(i) = rand()%10;
 		for(int j=0;j<3;j++){
@@ -19,6 +24,8 @@ int main(int argc, char **argv) {
 	}
 	
 	cout<<v;
+	v=v*10;
+	cout<<v;
 	cout<<m1 + m2;
 	cout<<m1-m2<<endl;
 	cout<<m1*m2<<endl;
@@ -26,6 +33,5 @@ int main(int argc, char **argv) {
 	
 	
 	cout<< m1 * v;
-	
 	return 0;
 }
