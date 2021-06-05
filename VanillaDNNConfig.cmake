4bb6d8f1a774a57a3276ac44f432c704ab67efe4
김재겸 <asbsds123@gmail.com>
2021-06-05 08:35:06 +0000

    Add {Source}

--- VanillaDNNConfig.cmake
+++ VanillaDNNConfig.cmake
@@ -1,6 +1,8 @@
 cmake_minimum_required(VERSION 3.10)
 
+
 set(MNIST_DATA_DIR ${CMAKE_CURRENT_LIST_DIR}/includes/MNIST/data)
-set(VanillaDNN_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/includes)
+set(VanillaDNN_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/Includes)
+set(VanillaDNN_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/Sources)
 
 
