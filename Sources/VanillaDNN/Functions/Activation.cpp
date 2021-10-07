2e086e59d97e2c7effdf071394af94e6a210f1d5
김재겸 <asbsds123@gmail.com>
2021-10-07 13:52:11 +0000

    Change Activation Function to Class

--- Sources/VanillaDNN/Functions/Activation.cpp
+++ Sources/VanillaDNN/Functions/Activation.cpp
@@ -3,15 +3,59 @@
 
 #include <VanillaDNN/Functions/Activation.hpp>
 
-Vector<float> ACTIVATION_FUNCTION::sigmoid(Vector<float>& input) {
+
+
+// Sigmoid
+Vector<float> Sigmoid::getActivated(Vector<float>& input){
 	Vector<float> output(input.get_size(), 0);
+	input = input.clip(-88.72, 88.72);
 	for (int i = 0; i < output.get_size(); i++) {
 		output(i) = 1 / (1 + exp(-input(i)));
 	}
 	return output;
 }
 
-Vector<float> ACTIVATION_FUNCTION::hyper_tan(Vector<float>& input) {
+Matrix<float> Sigmoid::getActivated(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	input = input.clip(-88.72, 88.72);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) = 1 / (1 + exp(-input(i, j)));
+		}
+	}
+	return output;
+	
+}
+Vector<float> Sigmoid::getActivatedDiff(Vector<float>& input){
+	Vector<float> output(input.get_size(), 0);
+	input = input.clip(-88.72, 88.72);
+	
+	Vector<float> temp = this->getActivated(input);
+	for (int i = 0; i < input.get_size(); i++) {
+		output(i) = temp(i) * (1 - temp(i));
+	}
+	
+	return output;
+}
+
+Matrix<float> Sigmoid::getActivatedDiff(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	input = input.clip(-88.72, 88.72);
+	
+	Matrix<float> temp = this->getActivated(input);
+	
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) = temp(i, j) * (1 - temp(i, j));
+		}
+	}
+	
+	return output;
+}
+
+
+// HyperTan
+Vector<float> HyperTan::getActivated(Vector<float>& input){
 	Vector<float> output(input.get_size(), 0);
 	for (int i = 0; i < output.get_size(); i++) {
 		output(i) = tanh(input(i));
@@ -19,7 +63,40 @@ Vector<float> ACTIVATION_FUNCTION::hyper_tan(Vector<float>& input) {
 	return output;
 }
 
-Vector<float> ACTIVATION_FUNCTION::ReLU(Vector<float>& input) {
+Matrix<float> HyperTan::getActivated(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) = tanh(input(i, j));
+		}
+	}
+	return output;
+	
+}
+
+Vector<float> HyperTan::getActivatedDiff(Vector<float>& input){
+	Vector<float> output(input.get_size(), 0);
+	Vector<float> temp = this->getActivated(input);
+	for (int i = 0; i < input.get_size(); i++) {
+		output(i) = 1 - (temp(i) * temp(i));
+	}
+	return output;
+}
+
+Matrix<float> HyperTan::getActivatedDiff(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	Matrix<float> temp = this->getActivated(input);
+	
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) = 1 - (temp(i, j) * temp(i, j));
+		}
+	}
+	return output;
+}
+
+// ReLU
+Vector<float> ReLU::getActivated(Vector<float>& input){
 	Vector<float> output(input.get_size(), 0);
 	for (int i = 0; i < output.get_size(); i++) {
 		output(i) = std::max(input(i), 0.0f);
@@ -27,46 +104,18 @@ Vector<float> ACTIVATION_FUNCTION::ReLU(Vector<float>& input) {
 	return output;
 }
 
-Vector<float> ACTIVATION_FUNCTION::leaky_ReLU(Vector<float>& input) {
-	Vector<float> output(input.get_size(), 0);
-	for (int i = 0; i < output.get_size(); i++) {
-		output(i) = 0 > input(i) ? 0.01 * input(i) : input(i);
+Matrix<float> ReLU::getActivated(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) = std::max(input(i, j), 0.0f);
+		}
 	}
 	return output;
+	
 }
 
-Vector<float> ACTIVATION_FUNCTION::soft_max(Vector<float>& input) {
-	Vector<float> output(input.get_size(), 0);
-	float sum = 0;
-	for (int i = 0; i < output.get_size(); i++) {
-		output(i) = exp(input(i));
-		sum += output(i);
-	}
-	return output / sum;
-}
-
-
-
-//Derivative of a function
-Vector<float> DIFF_FUNCTION::sigmoid_diff(Vector<float>& input) {
-	Vector<float> output(input.get_size(), 0);
-	Vector<float> temp = ACTIVATION_FUNCTION::sigmoid(input);
-	for (int i = 0; i < input.get_size(); i++) {
-		output(i) = temp(i) * (1 - temp(i));
-	}
-	return output;
-}
-
-Vector<float> DIFF_FUNCTION::hyper_tan_diff(Vector<float>& input) {
-	Vector<float> output(input.get_size(), 0);
-	Vector<float> temp = ACTIVATION_FUNCTION::hyper_tan(input);
-	for (int i = 0; i < input.get_size(); i++) {
-		output(i) = 1 - (temp(i) * temp(i));
-	}
-	return output;
-}
-
-Vector<float> DIFF_FUNCTION::ReLU_diff(Vector<float>& input) {
+Vector<float> ReLU::getActivatedDiff(Vector<float>& input){
 	Vector<float> output(input.get_size(), 0);
 	for (int i = 0; i < output.get_size(); i++) {
 		output(i) = static_cast<int>(0.0f < input(i));
@@ -74,7 +123,37 @@ Vector<float> DIFF_FUNCTION::ReLU_diff(Vector<float>& input) {
 	return output;
 }
 
-Vector<float> DIFF_FUNCTION::leaky_ReLU_diff(Vector<float>& input) {
+Matrix<float> ReLU::getActivatedDiff(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i,j) = static_cast<int>(0.0f < input(i,j));
+		}
+	}
+	return output;
+}
+
+// LeakyReLU
+Vector<float> LeakyReLU::getActivated(Vector<float>& input){
+	Vector<float> output(input.get_size(), 0);
+	for (int i = 0; i < output.get_size(); i++) {
+		output(i) = 0 > input(i) ? 0.01 * input(i) : input(i);
+	}
+	return output;
+}
+
+Matrix<float> LeakyReLU::getActivated(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) =  0 > input(i, j) ? 0.01 * input(i, j) : input(i, j);
+		}
+	}
+	return output;
+	
+}
+
+Vector<float> LeakyReLU::getActivatedDiff(Vector<float>& input){
 	Vector<float> output(input.get_size(), 0);
 	for (int i = 0; i < output.get_size(); i++) {
 		output(i) = 0 > input(i) ? 0.01 : 1;
@@ -82,17 +161,41 @@ Vector<float> DIFF_FUNCTION::leaky_ReLU_diff(Vector<float>& input) {
 	return output;
 }
 
-
-Vector<float> DIFF_FUNCTION::soft_max_diff(Vector<float>& input){
-	Vector<float> output(input.get_size(), 0);
-	Vector<float> temp = ACTIVATION_FUNCTION::soft_max(input);
-	for (int i = 0; i < input.get_size(); i++) {
-		for(int j = 0; j < input.get_size(); j++){
-			output(i) += temp(i)*((static_cast<int>(i==j)) - temp(j));
+Matrix<float> LeakyReLU::getActivatedDiff(Matrix<float>& input){
+	Matrix<float> output(input.get_rows_size(), input.get_cols_size(), 0);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) =  0 > input(i, j) ? 0.01 : 1;
 		}
 	}
 	return output;
 }
+
+
+// SoftMax
+Vector<float> SoftMax::getActivated(Vector<float>& input){
+	Vector<float> output(input.get_size(), 0);
+	input = input.clip(-88.72, 88.72);
+	float sum = 0;
+	for (int i = 0; i < output.get_size(); i++) {
+		output(i) = exp(input(i));
+		sum += output(i);
+	}
+	return output / sum;
+	
+}
+
+Matrix<float> SoftMax::getActivatedDiff2(Vector<float>& input){
+	Vector<float> temp = this->getActivated(input);
+	Matrix<float> output(temp.get_size(), temp.get_size(), 0);
+	for (int i = 0; i < output.get_rows_size(); i++) {
+		for(int j = 0; j < output.get_cols_size(); j++){
+			output(i, j) =  temp(i) * (static_cast<int>(i==j) - temp[j]);
+		}
+	}
+	return output;
+}
+
 
 
 #endif
